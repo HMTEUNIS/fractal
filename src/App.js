2cae6534ba0387ef319ceff3a39d53e7826bedeb
holz <hmt60659@protonmail.com>
2024-09-08 01:17:45 -0700

    all done for today

--- src/App.js
+++ src/App.js
@@ -2,13 +2,82 @@ import logo from './logo.svg';
 import './App.css';
 import * as ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
-import {useState, useCallback} from 'react'
+import {useState, useCallback, useEffect} from 'react'
+import Typewriter from 'typewriter-effect';
 import React from 'react';
 function Home(){
+  const [typewrite, setType] = useState({index: 0, line: 0, text: ""})
+
 
 
   return(
-    <div>FRACTAL is a dream of worldmaking through ongoing processes of joyful learning, a radical non-hierarchical collective study convergence for experimenting towards thriving futures, a pluripotent seed for queer and liberatory futures, a portal for summoning desirable worlds. We’re gathering in Tovaangar/so called Los Angeles from Nov 22-25th, weaving together constellations of our beloved and brilliant friends, at the leading edges, in the shadowy undergrounds, fugitive cracks, and entangled middles, of art, ecology and technology, for a moment of shared inquiry, cocreation, nourishment and to stir up good trouble. Want to come play with us?</div>
+    <div id="home_page">
+
+      <div>
+
+{/* <Typewriter
+  options={{
+    strings: ['FRACTAL is a dream of worldmaking through ongoing processes of joyful learning,' ,
+      'a radical non-hierarchical collective study convergence for experimenting towards thriving futures,'
+      ,
+      'a pluripotent seed for queer and liberatory futures, a portal for summoning desirable worlds.' 
+      ,
+      'We’re gathering in Tovaangar (so called Los Angeles)from Nov 22-25th,'
+      ,
+      'and weaving together constellations of our beloved and brilliant friends,'
+      ,
+      'at the leading edges, in the shadowy undergrounds, fugitive cracks, and entangled middles, of art, ecology and technology, for a moment of shared inquiry, cocreation, nourishment and to stir up good trouble.'
+      ,
+      'Want to come play with us?'
+      ],
+    autoStart: true,
+    loop: true,
+
+  }}
+/> */}
+
+
+<Typewriter
+  onInit={(typewriter) => {
+    typewriter
+    .typeString('FRACTAL is a dream of worldmaking through ongoing processes of joyful learning,')
+      .pauseFor(250)
+      .typeString('<br/> a pluripotent seed for queer and liberatory futures,')
+      .pauseFor(250)
+      .typeString('<br/> a portal for summoning desirable worlds.')
+      
+      .pauseFor(250)
+      .typeString('<br/> We’re gathering in Tovaangar (so called Los Angeles)from Nov 22-25th,')
+      .pauseFor(250)
+      .typeString('<br/> and weaving together constellations of our beloved and brilliant friends,')
+      .pauseFor(250)
+      .typeString('<br/> at the leading edges,')
+      .pauseFor(150)
+      .typeString('<br/> in the shadowy undergrounds,')
+      .pauseFor(150)
+      .typeString('<br/>  fugitive cracks,')
+      .pauseFor(150)
+      .typeString('<br/>  and entangled middles,')
+      .pauseFor(150)
+      .typeString('<br/>  of art, ecology and technology,')
+      .pauseFor(150)
+      .typeString('<br/>   for a moment of shared inquiry, cocreation, nourishment and to stir up good trouble.')
+      .pauseFor(250)
+      .typeString('<br/>   Want to come play with us?')
+      
+      
+      .start();
+  }}
+/>
+
+
+
+
+      </div>
+
+
+
+        </div>
   )
 }
 
@@ -24,50 +93,59 @@ const errors = {
   return(
     <div>
       <div>
-      This Fall we are gathering in Tovaangar, so called Los Angeles, from November 22-25th. Weaving together constellations of our beloved and brilliant friends, at the leading edges, in the shadowy undergrounds, fugitive cracks, and entangled middles, of art, ecology and technology (and more!), for a moment of shared inquiry, co-creation, nourishment and to stir up good trouble.
-We are seeking proposals from facilitators interested in joining us in our endevor. Be it a class, a workshop, a presntation, a collaboritive session, whatever makes sense for your vision, we want to provide you with a <i>container</i> to inhabit.
+     <p>This Fall we are gathering in Tovaangar, so called Los Angeles, from November 22-25th.</p> 
+      <p>
+      Weaving together constellations of our beloved and brilliant friends, at the leading edges, in the shadowy undergrounds, fugitive cracks, and entangled middles, of art, ecology and technology (and more!), for a moment of shared inquiry, co-creation, nourishment and to stir up good trouble.
+We are seeking proposals from facilitators interested in joining us in our endevor. Be it a class, a workshop, a presntation, a collaboritive session, whatever makes sense for your vision, we want to provide you with a <i>container</i> to fill as you see fit.</p>
+
       </div>
       <div> 
-        <form onSubmit={handleSubmit}>
-        <label>Name:</label>
+        <form onSubmit={handleSubmit}> 
+           <div id='basic_info'>
+        <label>Name: </label>
+      
       <input type="text" name="name" required />
       {errors.name && <div className="error">{errors.name}</div>}
 
-      <label>Email:</label>
+      <label>Email: </label>
       <input type="text" name="email" required />
       {errors.email && <div className="error">{errors.email}</div>}
 
-      <label>Phone #:</label>
+      <label>Phone: </label>
       <input type="text" name="phone" required />
       {errors.phone && <div className="error">{errors.phone}</div>}
-
-      <label>Any internet links / portfolio / website / social media of your previous work you’d like to share?</label>
-      <input type="text" placeholder="It's okay if not :)"name="web"  />
+</div>
+<div id='long_form_resp'>
+      <label>Any internet links / portfolio / website / social media of your previous work you’d like to share? S</label>
+      <input placeholder="It's okay if not :)"name="web"  />
       {errors.web && <div className="error">{errors.web}</div>}
+<br />
 
-
-      <label>Facilitator Bio </label>
-      <input type="text" name="bio" placeholder='Feel free to add any information you’d like shared about yourself relevant to your container offering or favorite fruit.' />
+      <label>Facilitator Bio: </label>
+     
+      <textarea type="text" class='long_form'name="bio" placeholder='Feel free to add any information you’d like shared about yourself relevant to your container offering or favorite fruit.' />
       {errors.web && <div className="error">{errors.web}</div>}
+      <br />
 
       <label>Container Title + Summary</label>
-      <input type="text" placeholder="Example: 'Transverse Portals' A course on navigating through and around the physical and mental barriers utilized and imposed by the security state. Making openings is an ancient technology for summoning and inducing flows both material and spiritual. How might we change our orientation to the world if we understand the inherent flaws of systems that prioritize security over collective wellbeing? How might the physical skill of lockpicking provide epistemic models for affecting systemic change?"name="title" required />
+      <textarea type="text" class='long_form' placeholder="Example: 'Transverse Portals' A course on navigating through and around the physical and mental barriers utilized and imposed by the security state. Making openings is an ancient technology for summoning and inducing flows both material and spiritual. How might we change our orientation to the world if we understand the inherent flaws of systems that prioritize security over collective wellbeing? How might the physical skill of lockpicking provide epistemic models for affecting systemic change?"name="title" required />
       {errors.title && <div className="error">{errors.title}</div>}
-
+      <br />
 
       <label>Container type? How do you envision this container offering?</label>
-      <input type="text" placeholder="We want to encourage facilitators to think expansively about their offerings, beyond a typical class or workshop, using Fractal as an opportunity to stretch your practice towards experimental ways of thinking and learning together. It’s cool/encouraged if it’s new for you too!
+      <textarea type="text" class='long_form' placeholder="We want to encourage facilitators to think expansively about their offerings, beyond a typical class or workshop, using Fractal as an opportunity to stretch your practice towards experimental ways of thinking and learning together. It’s cool/encouraged if it’s new for you too!
 - Container possibilities include: class // workshop // secret mission // ritual // field trip // play date // etc"name="type" required />
       {errors.type && <div className="error">{errors.type}</div>}
 
+      <br />
 
       <label>What activities will you facilitate and will there be any culminating projects? </label>
-      <input type="text" placeholder="A share out, performance, a bracelet with your best friends name on it"name="takeaway" required />
+      <textarea type="text" class='long_form'placeholder="A share out, performance, a bracelet with your best friends name on it"name="takeaway" required />
       {errors.takeaway && <div className="error">{errors.takeaway}</div>}
 
 
       <label>Resources // supplies you would need? Would you bring any supplies?</label>
-      <input type="text" placeholder="Electricity, Projector, Craft stuff, GIANT paper, 2000 gummy worms? "name="needs" required />
+      <textarea type="text" class='long_form' placeholder="Electricity, Projector, Craft stuff, GIANT paper, 2000 gummy worms? "name="needs" required />
       {errors.needs && <div className="error">{errors.needs}</div>}
 
       <label>Location</label>
@@ -79,12 +157,7 @@ We are seeking proposals from facilitators interested in joining us in our endev
       {errors.time && <div className="error">{errors.time}</div>}
 
       <label>s your container scalable for groups of different sizes? Is there an ideal group size or any size parameters?</label>
-      <input type="text" placeholder="how many people we talking here?"name="size" required />
-      {errors.size && <div className="error">{errors.size}</div>}
-
-
-      <label>Is your container scalable for groups of different sizes? Is there an ideal group size or any size parameters?</label>
-      <input type="text" placeholder="how many people we talking here?"name="size" required />
+      <textarea type="text" class='long_form' placeholder="how many people we talking here?"name="size" required />
       {errors.size && <div className="error">{errors.size}</div>}
 
 
@@ -94,18 +167,18 @@ We are seeking proposals from facilitators interested in joining us in our endev
 
 
       <label>Any community support needs?</label>
-      <input type="text" placeholder="Accommodations? Dietary restrictions? Allergies? Medical and Access support?"name="accomidations"  />
+      <textarea type="text" class='long_form' placeholder="Accommodations? Dietary restrictions? Allergies? Medical and Access support?"name="accomidations"  />
       {errors.accomidations && <div className="error">{errors.accomidations}</div>}
 
 
       <label>What are some aspects of group gatherings that sparked inspiration and excitement for you that you would like to see integrated?</label>
-      <input type="text" placeholder="Bulletin board for spontaneous activities, collective cooking and meal sharing, evening programming, community radio, secret message mailboxes etc."name="likes"  />
+      <textarea type="text" class='long_form' placeholder="Bulletin board for spontaneous activities, collective cooking and meal sharing, evening programming, community radio, secret message mailboxes etc."name="likes"  />
       {errors.likes && <div className="error">{errors.likes}</div>}
       
       <label>Any thing else you'd like to share?</label>
-      <input type="text" name="ect" />
+      <textarea type="text" class='long_form' name="ect" />
       {errors.ect && <div className="error">{errors.ect}</div>}
-
+</div>
           <button type='submit'>send it</button>
         </form>
 
@@ -118,10 +191,18 @@ We are seeking proposals from facilitators interested in joining us in our endev
   )
 }
 
+function News(){
+  console.log("beep")
+
+  return(
+    <div>Hang Tight!</div>
+  )
+}
 
   function App() {
    
     return (
+      <div id='god'>
       <div id="main_cont">
 <Router>
         <div id='top_bar'>
@@ -139,8 +220,11 @@ We are seeking proposals from facilitators interested in joining us in our endev
 </span>
         <div id="top_nav">
         
-       <NavLink to='/'> <span>Home</span> </NavLink >
-      <NavLink to='/submit_class'> <span>Submit Your Idea</span></NavLink>
+       <NavLink to='/'> <span>About</span> </NavLink >
+       <NavLink to='/news'> <span>News</span></NavLink>
+       <NavLink to='/register'> <span>Register</span></NavLink>
+
+      <NavLink to='/submit_class'> <span>Submit</span></NavLink>
         </div>
 
         </div>
@@ -148,12 +232,15 @@ We are seeking proposals from facilitators interested in joining us in our endev
    <div id='content_container'>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/news" element={<News />} />
+
           <Route path="/submit_class" element={<SubmitClass />} />
 
         </Routes>
       </div>   
       </Router>
 
+      </div>
       </div>
     );
   }
